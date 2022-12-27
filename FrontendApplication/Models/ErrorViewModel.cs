6539e12863fff5f6a01d2b697bfed8e16bc7259a
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-27 18:39:42 +0300

    Frontend Application

---
+++ FrontendApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FrontendApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
