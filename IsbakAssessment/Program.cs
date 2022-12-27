8c4aeb2b77daea715bf4100a07f70800e00c8ddf
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-27 17:55:35 +0300

    First commit.

---
+++ IsbakAssessment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IsbakAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
